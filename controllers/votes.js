c06b6406cc53f87df02bffe2fd6d0983ea18efa5
Julian <julian.franco@icloud.com>
2023-06-04 18:21:13 -0400

    update castVote fn

--- controllers/votes.js
+++ controllers/votes.js
@@ -1,10 +1,29 @@
-const { Vote } = require('../models')
+const { Vote, Movie } = require('../models')
 
 async function castVote(req, res) {
   try {
 		req.body.voterId = req.user.profile.id
-    const vote = await Vote.create(req.body)
-    res.status(200).json(vote)
+
+    const prevVote = await Vote.findOne({
+      where: {
+        voterId: req.body.voterId,
+        movieId: req.body.movieId,
+      }
+    })
+
+    if (prevVote) {
+      prevVote.value = req.body.value
+      await prevVote.save()
+    } else {
+      await Vote.create(req.body)
+    }
+
+    const movie = await Movie.findByPk(
+      req.body.movieId,
+      { include: [{ model: Vote, as: "votesReceived" }] }
+    )
+
+    res.status(200).json(movie)
   } catch (error) {
     res.status(500).json({ err: error })
   }
